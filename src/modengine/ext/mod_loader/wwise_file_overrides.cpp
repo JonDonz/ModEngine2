dc9a3564da1574ef8c3591ec2c244a0a1a10c23d
jeffingraham <ingraham.jeff@gmail.com>
2024-09-15 23:00:36 -0700

    Removing extra space

--- src/modengine/ext/mod_loader/wwise_file_overrides.cpp
+++ src/modengine/ext/mod_loader/wwise_file_overrides.cpp
@@ -77,7 +77,7 @@ std::optional<std::wstring> normalize_filename(const std::wstring path) {
     if (path.starts_with(L"sd:/")) {
         return std::wstring(path.substr(4));
     } else if (path.starts_with(L"sd_dlc02:/")) {
-        return  std::wstring(path.substr(10));
+        return std::wstring(path.substr(10));
     }
 
     return {};
